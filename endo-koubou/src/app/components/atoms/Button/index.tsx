63fd09281b0a3b9c2c51ade61763623ef67f1a06
PK-Yakkun <mokkun515@gmail.com>
2023-09-20 00:48:26 +0900

    feat: Buttonコンポーネント改修 サイズ、角丸、BgColorのprops追加

--- endo-koubou/src/app/components/atoms/Button/index.tsx
+++ endo-koubou/src/app/components/atoms/Button/index.tsx
@@ -2,14 +2,28 @@ import styles from "./button.module.scss";
 
 export type ButtonProps = {
   color?: "base" | "primary";
+  bgColor?: "primary" | "secondary";
+  size?: "s" | "m" | "l";
+  rounded?: boolean;
   children?: React.ReactNode;
 };
 
-export function Button({ color, children }: ButtonProps) {
+export function Button({
+  color,
+  bgColor,
+  size,
+  rounded,
+  children,
+}: ButtonProps) {
   const colorClass = `color_${color}`;
+  const bgClass = bgColor ? `bg_${bgColor}` : "";
+  const sizeClass = size ? `size_${size}` : "";
+  const roundedClass = rounded ? "rounded" : "";
 
   return (
-    <button className={`${styles.button} ${styles[colorClass]}`}>
+    <button
+      className={`${styles.button} ${styles[colorClass]} ${styles[bgClass]} ${styles[sizeClass]} ${styles[roundedClass]}`}
+    >
       {children}
     </button>
   );
