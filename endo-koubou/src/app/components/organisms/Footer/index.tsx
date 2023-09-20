c6b4c2f0813fd0e008c6eb94a0a3c537750009ba
PK-Yakkun <mokkun515@gmail.com>
2023-09-20 23:52:46 +0900

    add: Footerコンポーネント作成

---
+++ endo-koubou/src/app/components/organisms/Footer/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./footer.module.scss";
+
+export function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.inner}>aa</div>
+    </footer>
+  );
+}
