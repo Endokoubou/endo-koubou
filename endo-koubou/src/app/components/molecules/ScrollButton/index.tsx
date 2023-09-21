c3698fcdfe833b4b308410b5e472ba7f106b45cd
PK-Yakkun <mokkun515@gmail.com>
2023-09-21 21:10:24 +0900

    feat: ScrollButtonにuse clientを適応、スムーススクロール実装

--- endo-koubou/src/app/components/molecules/ScrollButton/index.tsx
+++ endo-koubou/src/app/components/molecules/ScrollButton/index.tsx
@@ -1,10 +1,18 @@
+"use client";
 import { Button, Typography } from "@/app/components/atoms";
 import { FaArrowUp } from "react-icons/fa6";
 import styles from "./scroll_button.module.scss";
 
 export function ScrollButton() {
+  const handleScrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
-    <Button>
+    <Button onClick={handleScrollToTop}>
       <div className={styles.wrapper}>
         <div className={styles.icon}>
           <FaArrowUp />
