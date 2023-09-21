3211e74621ff10b3c1b0d3dd8150f15a12b90655
PK-Yakkun <mokkun515@gmail.com>
2023-09-21 21:19:49 +0900

    add: Logoコンポーネント作成

--- endo-koubou/src/app/components/atoms/index.ts
+++ endo-koubou/src/app/components/atoms/index.ts
@@ -1,5 +1,6 @@
 import { Button } from "./Button";
 import { Typography } from "./Typography";
 import { FadeIn } from "./FadeIn";
+import { Logo } from "./Logo";
 
-export { Button, Typography, FadeIn };
+export { Button, Typography, FadeIn, Logo };
