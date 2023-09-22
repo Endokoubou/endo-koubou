a52c8df7a7ae67498cd6cbd665730e6f7ccfe9f2
PK-Yakkun <mokkun515@gmail.com>
2023-09-23 00:56:06 +0900

    feat: TOP contactセクションにReadMoreコンテンツ追加

--- endo-koubou/src/app/page.tsx
+++ endo-koubou/src/app/page.tsx
@@ -7,6 +7,7 @@ import {
   MvTitle,
   ContactButton,
   FlowItem,
+  ReadMore,
 } from "@/app/components/molecules";
 import { SnsButtons } from "@/app/components/organisms";
 
@@ -168,7 +169,44 @@ export default function Home() {
             <Typography variant="p">
               ご依頼前の相談のみも受け付けておりますので、お気軽にお問い合わせください。
             </Typography>
-            <div>setumei</div>
+            <ReadMore>
+              <>
+                <Typography variant="p">
+                  お問い合わせ時に必要な情報についてはこちらをご確認ください。
+                  <br />
+                  こちらが不足しておりますとご返答できかねる場合がございますので、必ず記載していただくようお願いいたします。
+                </Typography>
+                <ul>
+                  <li>
+                    <Typography variant="p">・お名前</Typography>
+                  </li>
+                  <li>
+                    <Typography variant="p">
+                      ・ご依頼内容、またはご相談内容
+                    </Typography>
+                  </li>
+                </ul>
+                <Typography variant="p">
+                  以下のような情報も可能な限り添えていただけますと、より詳細なご提案やご回答が行えますので、よろしければ記入していただけますと幸いです。
+                </Typography>
+                <ul>
+                  <li>
+                    <Typography variant="p">・所属団体</Typography>
+                  </li>
+                  <li>
+                    <Typography variant="p">・依頼したい公演の情報</Typography>
+                  </li>
+                  <li>
+                    <Typography variant="p">
+                      ・スケジュールやご予算などご依頼に付随する情報
+                    </Typography>
+                  </li>
+                  <li>
+                    <Typography variant="p">・その他質問事項など</Typography>
+                  </li>
+                </ul>
+              </>
+            </ReadMore>
             <div className={styles.button}>
               <ContactButton label="お問い合わせ" />
             </div>
