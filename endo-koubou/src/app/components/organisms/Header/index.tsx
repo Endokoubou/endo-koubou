60250461fffb709dd56a35e1c355891adf4fd2eb
PK-Yakkun <mokkun515@gmail.com>
2023-09-23 02:27:56 +0900

    feat: Headerコンポーネントにprops追加、アニメーションor固定か選択できる機能追加

--- endo-koubou/src/app/components/organisms/Header/index.tsx
+++ endo-koubou/src/app/components/organisms/Header/index.tsx
@@ -4,34 +4,45 @@ import { useState, useEffect } from "react";
 import { Logo, Typography } from "@/app/components/atoms";
 import styles from "./header.module.scss";
 
+export type HeaderProps = {
+  isFixed?: boolean;
+};
+
 const menuMapping = [
   { label: "作品一覧", path: "/art-work" },
   { label: "会社概要 & スタッフ一覧", path: "/company" },
   { label: "お問い合わせ", path: "/contact" },
 ];
 
-export function Header() {
+export function Header({ isFixed = false }: HeaderProps) {
   const [showHeader, setShowHeader] = useState(false);
+  const fixedClass = isFixed ? "fixed" : "animation";
 
   useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollY = window.scrollY;
-      if (currentScrollY > 455) {
-        setShowHeader(true);
-      } else {
-        setShowHeader(false);
-      }
-    };
-    window.addEventListener("scroll", handleScroll);
+    if (isFixed) {
+      return;
+    } else {
+      const handleScroll = () => {
+        const currentScrollY = window.scrollY;
+        if (currentScrollY > 455) {
+          setShowHeader(true);
+        } else {
+          setShowHeader(false);
+        }
+      };
+      window.addEventListener("scroll", handleScroll);
 
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+    }
+  }, [isFixed]);
 
   return (
     <header
-      className={`${styles.header} ${showHeader ? styles.slideDown : ""}`}
+      className={`${styles.header} ${styles[fixedClass]} ${
+        !isFixed && showHeader && styles.slideDown
+      }`}
     >
       <div className={styles.inner}>
         <Link href="/">
