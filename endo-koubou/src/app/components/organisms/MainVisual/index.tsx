71a51e7daac88766fdf9619cb3a8317ade8f9df6
PK-Yakkun <mokkun515@gmail.com>
2023-09-27 23:15:22 +0900

    add: MainVisualコンポーネント作成

---
+++ endo-koubou/src/app/components/organisms/MainVisual/index.tsx
@@ -0,0 +1,57 @@
+"use client";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { FadeIn } from "@/app/components/atoms";
+import { MvTitle } from "@/app/components/molecules";
+import styles from "./main_visual.module.scss";
+
+const images = [
+  "/images/mv_01.jpg",
+  "/images/mv_02.jpg",
+  "/images/mv_03.jpg",
+  "/images/mv_04.jpg",
+];
+
+export function MainVisual() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setCurrentSlide((prevSlide) => (prevSlide + 1) % images.length);
+    }, 6000);
+
+    return () => clearTimeout(timer);
+  }, [currentSlide]);
+
+  return (
+    <section className={styles.mv} style={{ height: "100vh" }}>
+      <div className={styles.title}>
+        <FadeIn fadeType="in">
+          <Image
+            src="/images/logo_endo-koubou.png"
+            width={75}
+            height={68}
+            alt="ENDO工房のロゴ画像"
+            className={styles.logo}
+          />
+        </FadeIn>
+        <MvTitle />
+      </div>
+      {images.map((image, index) => (
+        <div
+          key={image}
+          className={
+            index === currentSlide ? styles.activeSlide : styles.inactiveSlide
+          }
+        >
+          <Image
+            src={image}
+            alt={`Slide ${index + 1}`}
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+      ))}
+    </section>
+  );
+}
--- endo-koubou/src/app/components/organisms/index.ts
+++ endo-koubou/src/app/components/organisms/index.ts
@@ -2,5 +2,6 @@ import { Header } from "./Header";
 import { Footer } from "./Footer";
 import { SnsButtons } from "./SnsButtons";
 import { ContactSection } from "./ContactSection";
+import { MainVisual } from "./MainVisual";
 
-export { Header, Footer, SnsButtons, ContactSection };
+export { Header, Footer, SnsButtons, ContactSection, MainVisual };
