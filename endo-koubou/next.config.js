9b09fd9cb940e375727b31f0740c82a4ae823f7e
PK-Yakkun <mokkun515@gmail.com>
2023-11-08 22:44:27 +0900

    feat: microCMSから画像を取得できるようconfig改修

--- endo-koubou/next.config.js
+++ endo-koubou/next.config.js
@@ -5,6 +5,14 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "app")],
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.microcms-assets.io",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
