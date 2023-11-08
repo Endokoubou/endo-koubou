56f3397f8e5507119af9394d876c6c97c55beb47
PK-Yakkun <mokkun515@gmail.com>
2023-11-09 01:32:17 +0900

    update: 作品詳細ページ 動的生成するように改修

--- endo-koubou/src/app/(sub_pages)/works/[id]/page.tsx
+++ endo-koubou/src/app/(sub_pages)/works/[id]/page.tsx
@@ -1,3 +1,27 @@
-export default async function WorkDetail() {
-  return <></>;
+import { getWorks } from "@/app/api/works";
+import { getWorkDetail } from "@/app/api/works/[id]";
+import { PageTitle } from "@/app/components/molecules";
+
+export async function generateStaticParams() {
+  const works = await getWorks();
+  const detailPaths = works.map((work) => work.id);
+  return detailPaths;
+}
+
+async function getDetail(id: string) {
+  const res = await getWorkDetail(id);
+  return res;
+}
+
+export default async function WorkDetail(props: {
+  params: { id: string };
+  searchParams: {};
+}) {
+  const detail = await getDetail(props.params.id);
+
+  return (
+    <>
+      <PageTitle title={detail.title} />
+    </>
+  );
 }
