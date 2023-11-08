7c2618e1e0412c5be6f6401aeb87746cd9dee7f5
PK-Yakkun <mokkun515@gmail.com>
2023-11-09 00:09:36 +0900

    feat: /worksページ、API疎通

--- endo-koubou/src/app/(sub_pages)/works/page.tsx
+++ endo-koubou/src/app/(sub_pages)/works/page.tsx
@@ -1,35 +1,14 @@
-import { PageTitle, WorkCard } from "@/app/components/molecules";
+import { getWorks } from "@/app/api/works";
+import { PageTitle } from "@/app/components/molecules";
 import { WorksSection } from "@/app/components/organisms";
-import styles from "./page.module.scss";
 
-const exampleWorks = {
-  works: [
-    {
-      title: "作品タイトル",
-      programName: "Eプロジェクト",
-      date: "2023年",
-      imagePath: "/",
-      imageWidth: "200px",
-      imageHeight: "100px",
-      alt: "alt",
-    },
-    {
-      title: "作品タイトル2",
-      programName: "Eプロジェクト",
-      date: "2023年",
-      imagePath: "/",
-      imageWidth: "200px",
-      imageHeight: "100px",
-      alt: "alt",
-    },
-  ],
-};
+export default async function Works() {
+  const data = await getWorks();
 
-export default function Works() {
   return (
     <>
       <PageTitle title="作品一覧" subtitle="WORKS" />
-      <WorksSection works={exampleWorks.works} />
+      <WorksSection works={data} />
     </>
   );
 }
