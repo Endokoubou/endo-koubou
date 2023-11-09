7a0500036eda0b6df1abff2224737ced7cebc84a
PK-Yakkun <mokkun515@gmail.com>
2023-11-10 01:33:30 +0900

    fix: Breadcrumbs SPA遷移のとき不具合があったためシンプルな構造に変更

--- endo-koubou/src/app/components/molecules/Breadcrumb/index.tsx
+++ endo-koubou/src/app/components/molecules/Breadcrumb/index.tsx
@@ -1,47 +1,32 @@
-"use client";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import { Typography } from "../../atoms";
-import styles from "./breadcrumb.module.scss"; // CSSモジュールを使う場合
+import styles from "./breadcrumb.module.scss";
 
-type SiteMap = { label: string; path: string }[];
-
-const siteMap: SiteMap = [
-  { label: "作品一覧", path: "/works" },
-  { label: "作品詳細", path: "/works/{id}" },
-  { label: "会社概要 & スタッフ一覧", path: "/company" },
-  { label: "お問い合わせ", path: "/contact" },
-];
-
-const generateBreadCrumbs = (path: string) => {
-  const breadcrumbs = [{ label: "TOP", path: "/" }];
-  const currentItem = siteMap.find((item) => item.path === path);
-  if (currentItem) {
-    breadcrumbs.push(currentItem);
-  }
-  if (breadcrumbs.length > 1) {
-    breadcrumbs[breadcrumbs.length - 1].path = "";
-  }
-  return breadcrumbs;
+type BreadcrumbProps = {
+  list: { label: string; path?: string }[];
 };
 
-export function Breadcrumb() {
-  const path = usePathname();
-  const breadcrumbs = generateBreadCrumbs(path);
-
+export function Breadcrumb({ list }: BreadcrumbProps) {
   return (
     <div className={styles.wrapper}>
       <nav className={styles.breadcrumb} aria-label="breadcrumb">
         <ol>
-          {breadcrumbs.map((breadcrumb, index) => (
-            <li key={index}>
-              {breadcrumb.path ? (
-                <Link href={breadcrumb.path}>{breadcrumb.label}</Link>
-              ) : (
-                <Typography variant="span">{breadcrumb.label}</Typography>
-              )}
-            </li>
-          ))}
+          <li>
+            <Link href="/">TOP</Link>
+          </li>
+          {list.map((item, index) =>
+            item.path ? (
+              <li key={index}>
+                <Link href={item.path}>
+                  <Typography variant="span">{item.label}</Typography>
+                </Link>
+              </li>
+            ) : (
+              <li key={index}>
+                <Typography variant="span">{item.label}</Typography>
+              </li>
+            )
+          )}
         </ol>
       </nav>
     </div>
