0a3a269ffc8f1a08c63ee3da7c1449dd04ea1bd9
PK-Yakkun <mokkun515@gmail.com>
2023-11-10 22:32:29 +0900

    update: /works レスポンシブ対応

--- endo-koubou/src/app/(sub_pages)/works/page.tsx
+++ endo-koubou/src/app/(sub_pages)/works/page.tsx
@@ -1,14 +1,17 @@
 import { getWorks } from "@/app/api/works";
 import { Breadcrumb, PageTitle } from "@/app/components/molecules";
 import { WorksSection } from "@/app/components/organisms";
+import styles from "./page.module.scss";
 
 export default async function Works() {
   const data = await getWorks();
 
   return (
     <>
-      <Breadcrumb list={[{ label: "作品一覧" }]} />
-      <PageTitle title="作品一覧" subtitle="WORKS" />
+      <div className={styles.head_contents}>
+        <Breadcrumb list={[{ label: "作品一覧" }]} />
+        <PageTitle title="作品一覧" subtitle="WORKS" />
+      </div>
       <WorksSection works={data} />
     </>
   );
